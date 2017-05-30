37dda8d2d168f0ec02214eac10a40edfb1c8ece3
Colin Jaffe <balloonasaurus@gmail.com>
2017-05-30 16:40:10 -0400

    adjusted the y scale to fit the mockup more closely

--- index.js
+++ index.js
@@ -1,71 +1,106 @@
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var padding = 1;
-var svg = d3.select('svg');
-var margin = {
-  top: 20,
-  right: 20,
-  bottom: 30,
-  left: 50
-};
-var width = +svg
-  .attr('width') - margin.left - margin.right;
-var height = +svg
-  .attr('height') - margin.top - margin.bottom;
-var g = svg.append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+var parseAskTime = d3.timeParse('%H:%M:%S');
+var parseTradeTime = d3.timeParse('%H:%M')
 
-var scaleX = d3.scaleBand()
+var scaleX = d3.scaleTime()
   .rangeRound([0, width]);
 
 var scaleY = d3.scaleLinear()
   .rangeRound([height, 0]);
 
-d3.json('stock.json', function(error, data) {
+var askArea = d3.area()
+  .x(function(d) {
+    return scaleX(d['timeStr']);
+  })
+  .y1(0)
+  .curve(d3.curveStepAfter);
+
+var bidArea = d3.area()
+  .x(function(d) {
+    return scaleX(d['timeStr'])
+  })
+  .y1(function(d) {
+    return scaleY(d['bid']);
+  })
+  .curve(d3.curveStepAfter);
+
+d3.json("stock.json", function(error, data) {
   if (error) throw error;
 
-  var filteredData = data['tradeList']
-    .slice(0, 20)
-    .map(function(trade) {
-      return trade['shares'];
-    });
+  var bboList = data['bboList'].map(function(bbo) {
+    bbo['timeStr'] = bbo['timeStr'].slice(0, 8);
+    bbo['timeStr'] = parseAskTime(bbo['timeStr']);
+    return bbo;
+  });
 
-  var indices = filteredData.map(function(bar, index) {
-    return index;
-  })
+  var tradeList = data['tradeList'].map(function(trade) {
+    var minutes = (trade['time'] / 60000000000).toFixed();
+    var hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
+    var timeString = hours.toString() + ':' + minutes.toString();
+    trade['time'] = parseTradeTime(timeString);
+    return trade;
+  });
 
-  scaleX.domain(indices);
-  scaleY.domain([0, d3.max(filteredData, function(datum) {
-    return datum;
-  })]);
+  scaleX.domain(d3.extent(bboList, function(d) { return d['timeStr']; }));
+  scaleY.domain([227500, 240000]);
+
+  askArea.y0(function(d) {
+    return scaleY(d['ask']);
+  });
+  bidArea.y0(height);
 
   var xAxis = d3.axisBottom(scaleX);
-  var yAxis = d3.axisLeft(scaleY);
 
-  g.append('g')
-    .attr('class', 'axis axis--x')
-    .attr('transform', 'translate(0, ' + height + ')')
-    .call(xAxis);
-
-  g.append('g')
-    .attr('class', 'axis axis--y')
-    .call(yAxis);
-
-  var rects = g.selectAll('rect')
-    .data(filteredData)
-    .enter().append('rect');
-
-  rects
-    .attr('x', function(barData) {
-      var indices = barData.map
-      return scaleX(indices);
+  var yAxis = d3
+    .axisLeft(scaleY)
+    .tickFormat(function(number) {
+      return (number / 10000).toFixed(1);
     })
-    .attr('y', function(barData) {
-      return scaleY(barData);
+
+  g.append("path")
+    .datum(bboList)
+    .attr("fill", "#945E1E")
+    .attr("d", askArea);
+
+  g.append('path')
+    .datum(bboList)
+    .attr('fill', '#4C7249')
+    .attr('d', bidArea);
+
+  g.selectAll('circle')
+    .data(tradeList)
+    .enter().append('circle')
+    .attr('cx', function(trade) {
+      return scaleX(trade['time']);
     })
-    .attr('width', function(barData) {
-      return scaleX(indices);
+    .attr('cy', function(trade) {
+
+      return scaleY(trade['price']);
     })
-    .attr('height', function(barData) {
-      return height - scaleY(barData);
+    .attr('r', 1.5)
+    .attr('fill', function(trade) {
+      return trade['tradeType'] === 'E'
+        ? 'black'
+        : 'red';
     });
+
+  g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  g.append("g")
+      .call(yAxis)
+    .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Price ($)");
 });
